8ecbefe51ba38f2a7edf9e153f807f9c81c25484
wlfrii <longfei.wang@sjtu.edu.cn>
2022-07-21 21:45:01 +0800

    add layer_3d

---
+++ src/layer_texture3d.cpp
@@ -0,0 +1,6 @@
+#include "layer_texture3d.h"
+
+LayerTexture3D::LayerTexture3D()
+{
+
+}
---
+++ src/layer_texture3d.h
@@ -0,0 +1,11 @@
+#ifndef LAYERTEXTURE3D_H
+#define LAYERTEXTURE3D_H
+
+
+class LayerTexture3D
+{
+public:
+    LayerTexture3D();
+};
+
+#endif // LAYERTEXTURE3D_H
