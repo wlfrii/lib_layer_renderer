3f7f48b9e7220afcbffdddd90341d9bdf3b3f34f
wlfrii <longfei.wang@sjtu.edu.cn>
2023-01-20 12:29:47 +0800

    add arrow

---
+++ src/shapes/arrow_generator.cpp
@@ -0,0 +1,21 @@
+#include "arrow_generator.h"
+#include "cylinder_generator.h"
+#include "cone_generator.h"
+#include "generator_util.h"
+
+
+ArrowGenerator::ArrowGenerator(float length, float radius,
+                               float cone_height, float cone_radius,
+                               const glm::mat4& pose)
+{
+    CylinderGenerator axis(length, radius, pose);
+    glm::mat4 cone_pose = pose;
+    cone_pose[3][0] += length * cone_pose[2][0];
+    cone_pose[3][1] += length * cone_pose[2][1];
+    cone_pose[3][2] += length * cone_pose[2][2];
+    ConeGenerator cone(cone_height, cone_radius, cone_pose);
+    
+    _vertices.clear();
+    _vertices.insert(_vertices.end(), axis.vertices().begin(), axis.vertices().end());
+    _vertices.insert(_vertices.end(), cone.vertices().begin(), cone.vertices().end());
+}
---
+++ src/shapes/arrow_generator.h
@@ -0,0 +1,33 @@
+#ifndef ARROW_GENERATOR_H_LF
+#define ARROW_GENERATOR_H_LF
+#include "generator_base.h"
+
+
+/**
+ * @brief A class for generating vertices for arrow
+ *      /\              
+ *     /  \   cone_height
+ *    /____\  cone_radius
+ *     |  |
+ *     |  |    
+ *     |  |  length
+ *     |__|    
+ *       radius     
+ */
+class ArrowGenerator : public GeneratorBase
+{
+public:    
+    /**
+     * @brief Constructor of class ArrowGenerator
+     * @param length       The length of axis
+     * @param radius       The radius of axis
+     * @param cone_height  The height of axis cone
+     * @param cone_radius  The radius of axis cone
+     * @param pose         The pose of coordinate
+     */
+    ArrowGenerator(float length, float radius, float cone_height,
+                   float cone_radius, const glm::mat4& pose);
+
+};
+
+#endif // ARROW_GENERATOR_H_LF
